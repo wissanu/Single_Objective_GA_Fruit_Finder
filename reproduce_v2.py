f5a771d060d16456d7560061daabdf7eb2dff0b0
Wissanu <thungrut@gmail.com>
2020-08-10 16:09:03 +0700

    Update reproduce_v2.py

--- reproduce_v2.py
+++ reproduce_v2.py
@@ -3,7 +3,7 @@ from random import randint
 def single_point(a, b, gene_num):
     point = randint(0, gene_num - 1)
     a[:point], b[:point] = b[:point], a[:point]
-    return a, b
+    return a.copy(), b.copy()
 
 
 def mutation(chromosome, max_count_item, gene_num):
